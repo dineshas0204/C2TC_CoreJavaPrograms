6d15132bc6c8ee9a5d80b91f6ba3f7cc0ff6fe41
Dinesh A <dineshas0204@gmail.com>
2025-12-21 21:44:26 +0530

    Day 3

---
+++ src/prl/daythree/Executor_oppsconcept_three.java
@@ -0,0 +1,17 @@
+package prl.daythree;
+
+public class Executor_oppsconcept_three {
+
+	public static void main(String[] args) {
+		EncapsulationDemo ED = new EncapsulationDemo();
+		ED.setName("M S Dhoni");
+		ED.setAge(44);
+		ED.setJersyNo(7);
+	
+		System.out.println(ED);
+		
+		Executor_oppsconcept_three e = new Executor_oppsconcept_three();
+		System.out.println(e);
+	}
+
+}
